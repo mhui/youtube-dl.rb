69139fb4d5e8ef12581881bbab1a4fde6109f737
Matthew Hui <matthew.hui@instacart.com>
2020-06-21 15:45:26 -0700

    Update

--- lib/youtube-dl/version.rb
+++ lib/youtube-dl/version.rb
@@ -6,5 +6,5 @@
 module YoutubeDL
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '0.3.1.2020.05.29'.freeze
+  VERSION = '0.3.1.2020.06.16'.freeze
 end
